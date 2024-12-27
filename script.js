66b1217663e1672bcb8e101a1170fdd036c6b95a
Smaran Shaha <smaranshaha@gmail.com>
2024-12-27 16:58:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+
+const initialState = {
+  value: 0,
+};
+
+// create reducer function
+function counterReducer(state = initialState, action) {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  } else {
+    return state;
+  }
+}
+
+// Create store (with the corrected function name)
+const store = Redux.createStore(counterReducer);
+
+// Render function to update the UI
+const render = () => {
+  const state = store.getState();
+  counterEl.innerText = state.value.toString();
+};
+
+// Initial render and subscription to state changes
+render();
+store.subscribe(render);
+
+// Button click listeners
+incrementEl.addEventListener("click", () => {
+  store.dispatch({
+    type: "increment",
+  });
+});
+decrementEl.addEventListener("click", () => {
+  store.dispatch({
+    type: "decrement",
+  });
+});
